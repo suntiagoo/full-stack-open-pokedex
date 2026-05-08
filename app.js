cd3acc986f639592fd4a553d9c9e69532864c743
Santiago Sandoval <191775772+suntiagoo@users.noreply.github.com>
2026-05-08 09:18:39 +0300

    Exercises 11.12

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
+
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
 
 // app.get('/health', (req, res) => {
+//   // eslint-disable-next-line no-constant-condition
+//   if (true) throw 'error...  '
 //   res.send('ok')
 // })
 
-app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
-  res.send('ok')
-})
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`server started on port ${PORT}`)
-})
+  console.log(`server started on port ${PORT}`);
+});
