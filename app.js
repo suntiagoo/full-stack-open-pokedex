74f3a7e5ec1f2ca8583abca4088f00ddf51ccce8
Santiago Sandoval <191775772+suntiagoo@users.noreply.github.com>
2026-05-08 09:20:30 +0300

    Exercises 11.12 fix eslint

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
-app.get("/health", (req, res) => {
-  res.send("ok");
-});
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 // app.get('/health', (req, res) => {
 //   // eslint-disable-next-line no-constant-condition
@@ -18,5 +18,5 @@ app.get("/health", (req, res) => {
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
